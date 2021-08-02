896423c1e555d76c2ad5829b8aa769847f530a65
gkpatwardhan <gkpatwardhan@gmail.com>
2021-08-02 20:47:56 +0000

    Completed the home page. It should display top 100 posts, ordered by timestamp.

---
+++ public/js/signIn.js
@@ -0,0 +1,24 @@
+const signIn = () => {
+    console.log("Calling sign in");
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then(result => {
+            //Do something with the result
+            console.log(`result is ${result}`);
+            const credential = result.credential;
+            const token = credential.accessToken;
+
+            const user = result.user;
+
+            console.log(user.uid);
+            userId = user.uid;
+            window.location = 'homePage.html'; 
+        })
+        .catch(error => {
+            //Something bad happened :(
+            console.log(error);
+        });
+
+};
