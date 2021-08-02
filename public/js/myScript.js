ba5ec28e63da9a361376a4982594501aba7e861e
SenpaiPotato <minrulee8@gmail.com>
2021-08-02 21:51:46 +0000

    added page titles, semi-completed myPosts page

---
+++ public/js/myScript.js
@@ -0,0 +1,67 @@
+console.log("Home page script loaded");
+function creationTime(epoch) {
+    const dateObject = new Date(epoch);
+    return dateObject.toLocaleString();
+}
+
+function displayPost(post) {
+    console.log("Displaying post");
+    console.log(post);
+    var title = post["title"];
+    var content = post["content"].substring(0, 100) + "...";
+    const time = creationTime(post.timestamp);
+    console.log("Time: ");
+    console.log(time);
+    return `
+    <div class="card m-3">
+        <header class="card-header">
+            <p class="card-header-title">
+                ${title}
+            </p>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                ${content}
+                <br>
+                <time>${time}</time>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+window.onload = (event) => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+        googleUser = user;
+        getNotes(user.uid);
+        } else {
+        window.location = 'index.html'; // 'index.html' is the login page
+        }
+    });
+}
+
+const getNotes = (userId) => {
+  const notesRef = firebase.database().ref(`users/${userId}`).orderByChild('timestamp').limitToLast(100);
+  console.log("Getting notes");
+  notesRef.on('value', (snapshot) => {
+      console.log("get ref to database");
+    const data = snapshot.val();
+    let postsGUI = ``;
+    console.log("data");
+    console.log(data);
+    for(const userKey in data) {
+        const userData = data[userKey];
+        console.log("user data");
+        console.log(userData);
+        for(const postKey in userData) {
+            const post = userData[postKey];
+            console.log("post");
+            console.log(post);
+            postsGUI += displayPost(post)
+        }
+    }
+    // Inject our string of HTML into our viewNotes.html page
+    document.querySelector('#app').innerHTML = postsGUI;
+    });
+};
